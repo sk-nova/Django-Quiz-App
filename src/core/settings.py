d03a36d4bb34604c0fb84f170bc7c697f0d9d0a7
sk-nova <karimshadaab510@gmail.com>
2025-01-26 02:18:15 +0530

    Made changes for DEBUG and SECRET_KEY env variables

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w^g3p%ke^tvn&saa)!v^fgv8%9r_fxni8g@614#3fq)ryl@n(t'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
+
+print('DEBUG:', DEBUG)
+print('SECRET_KEY:', SECRET_KEY)
 
 
 # Application definition

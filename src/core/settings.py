fef3242039ee8fdbde9abd426aab511e8c016ca9
sk-nova <karimshadaab510@gmail.com>
2025-01-26 11:07:02 +0530

    Revert db engine back to sqlite

--- src/core/settings.py
+++ src/core/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": config('DATABASE_ENGINE'),
-        "NAME": config('DATABASE_NAME'),
-        "USER": config('DATABASE_USERNAME'),
-        "PASSWORD": config('DATABASE_PASSWORD'),
-        "HOST": config('DATABASE_HOST'),
-        "PORT": config('DATABASE_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
